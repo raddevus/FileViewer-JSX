85f155430fb378d57f0135a76f18a7872c3af6aa
raddevus <1radeutsch1@gmail.com>
2023-02-12 16:50:43 -0500

    Added ability for user to type a path and click the [Go] button to navigate directly to a directory. Works great.

--- FileViewer/UserInterface/src/App.js
+++ FileViewer/UserInterface/src/App.js
@@ -55,6 +55,15 @@ function App() {
         // to get serialized properly on the receiving side.
         window.external.sendMessage(`{"Command":"testMsg","Data":"Hi .NET! 🤖"}`);
         //window.external.sendMessage('Hi .NET! 🤖');
+
+    }
+
+    function goToDirectory(){
+        // Click Event for DIRECTORIES
+        let getPathData = {};
+        getPathData.Command="getPathData";
+        getPathData.Data = document.querySelector("#clearTextFilePath").value;
+        window.external.sendMessage(JSON.stringify(getPathData));
     }
     
     return (
@@ -62,7 +71,7 @@ function App() {
             <h2>Files</h2>
             <div class="input-group mb-3">
                 <input id="clearTextFilePath" type="text" class="form-control" placeholder="Clear-text file" aria-label="clear-text-file" aria-describedby="clearTextFileButton" />
-                <button class="btn btn-outline-secondary" type="button" id="clearTextFileButton">Button</button>
+                <button onClick={goToDirectory} class="btn btn-outline-secondary" type="button" id="clearTextFileButton">Go</button>
             </div>
                 <table id="fileSystem" class="table  table-striped table-hover">
                 <thead class="table-success">
